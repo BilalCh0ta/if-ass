e8d396b29887fa55bbe1c2c6ad0e50606c19adcd
BilalCh0ta <smit.11.aandw@gmail.com>
2024-03-01 00:25:35 +0500

    9Q

---
+++ app.js
@@ -0,0 +1,162 @@
+
+// question 1
+
+// var a = +prompt("enter number");
+// if (a % 3 === 0 ){
+//     console.log("This number is divisible by 3")
+// }
+
+// else{
+//     console.log("This number is not divisible by 3")
+// };
+
+// question 1
+
+
+
+
+// question 2
+
+// var b = +prompt("Enter number")
+//  if(b % 2 ===0){
+//     console.log("this is an even number")
+//  }
+//  else{
+//     console.log("this is an odd number")
+//  }
+
+// question 2
+
+
+// question 3
+
+// var age = +prompt("Enter ur age");
+
+// if(age >= 18){
+// console.log("Old enough")
+// }
+// else{
+//     console.log("Too young")
+// }
+
+
+// question 3
+
+
+
+
+// question 4
+
+// var names = prompt("Enter ur name");
+// if (names == "bilal"){
+//     console.log("hello bilal")
+// }
+// else{
+//     console.log(" ")
+// }
+
+// question 4
+
+
+
+
+// question 7
+
+// calculator 
+
+// var num1 = +prompt("enter num1");
+// var operator = prompt("choose operator");
+// var num2 = +prompt("enter num2");
+// var add = num1 + num2;
+// var sub = num1 - num2;
+// var product = num1 * num2;
+// var divide = num1 / num2;
+// var mode = num1 % num2 ;
+
+
+//  if(operator == "-"){
+//     console.log(sub)
+// }
+// else if(operator == "+"){
+//     console.log(add)
+// }
+// else if(operator == "*"){
+//     console.log(product)
+// }
+// else if(operator == "/"){
+//     console.log(divide)
+// }
+// else if(operator == "%"){
+//     console.log(mode)
+// }
+
+// else{
+//     console.log("nothing")
+// }
+
+// calculator 
+
+
+
+// question 7
+
+
+
+// question 8
+
+// var time = +prompt("enter time")
+
+// if (time >= 0 && time < 12  ){
+//     console.log("good morning")
+
+// }
+
+// else if(time  >= 12 && time < 17 ){
+//     console.log("good afternoon")
+// }
+
+// else if(time  >= 17 && time < 21 ){
+//     console.log("good evening")
+// }
+
+// else if(time  >= 21 && time < 24 ){
+//     console.log("good night")
+// }
+// else(
+//     console.log("nothing")
+// )
+
+// question 8
+
+
+
+// question 9
+
+// var year = +prompt("enter year and check it is leap year or not")
+
+// if (year % 4 ==0 ){
+//     console.log("leap year")
+// }
+// else{
+// console.log("normal year")
+
+// }
+
+// question 9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
